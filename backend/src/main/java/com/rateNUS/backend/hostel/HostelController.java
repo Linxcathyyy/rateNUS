a96826ecfb727c2983b7e0f46a824d16741ecfd1
SkyBlaise <zsc990124@gmail.com>
2021-12-06 16:27:27 +0800

    Allow GET request on individual hostel by ID

--- backend/src/main/java/com/rateNUS/backend/hostel/HostelController.java
+++ backend/src/main/java/com/rateNUS/backend/hostel/HostelController.java
@@ -2,6 +2,7 @@ package com.rateNUS.backend.hostel;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,10 @@ public class HostelController {
     @GetMapping
     public List<Hostel> getAllHostel() {
         return hostelService.getAllHostel();
+    }
+
+    @GetMapping(path = "{hostelId}")
+    public Hostel getHostel(@PathVariable("hostelId") long hostelId) {
+        return hostelService.getHostel(hostelId);
     }
 }
--- backend/src/main/java/com/rateNUS/backend/hostel/HostelService.java
+++ backend/src/main/java/com/rateNUS/backend/hostel/HostelService.java
@@ -4,6 +4,7 @@ import com.rateNUS.backend.util.DummyData;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Provides the services required by the API layer for Hostels.
@@ -12,5 +13,18 @@ import java.util.List;
 public class HostelService {
     public List<Hostel> getAllHostel() {
         return DummyData.hostelList;
+    }
+
+    public Hostel getHostel(long hostelId) {
+        Optional<Hostel> hostelOptional = getAllHostel().stream()
+                .filter(hostel -> hostel.getId() == hostelId)
+                .findFirst();
+
+        if (hostelOptional.isEmpty()) {
+            System.out.println("Hostel with ID \" + hostelId + \" does not exists.");
+            // throw new Exception("Hostel with ID " + hostelId + " does not exists.");
+        }
+
+        return hostelOptional.get();
     }
 }
