d25d1e7bee0ca7f3612bbeda6602061c5946a368
Lin Xingchen <linxingchen0428cathy@gmail.com>
2021-12-06 12:58:59 +0800

    Add HostelList.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HostelList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HostelList from './components/HostelList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HostelList
   }
 }
 </script>
---
+++ frontend/src/components/HostelList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Hostel List</h1>
+        <div v-for="hostel in hostelList" :key="hostel.id">
+            <h3>{{hostel.id}}. {{hostel.title}}</h3>
+            <p>{{hostel.body}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name:"HostelList",
+    data() {
+        return {
+            hostelList: [],
+        }
+    },
+    mounted() {
+        axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+            console.log(response.data);
+            this.hostelList = response.data.slice(0, 2);
+        }).catch(error => {
+            console.log(error);
+        })
+    },
+}
+</script>
+
+<style>
+
+</style>
