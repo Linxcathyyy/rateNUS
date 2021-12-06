35be9d46d91b172081b66309421d30c84722a870
Lin Xingchen <linxingchen0428cathy@gmail.com>
2021-12-06 13:46:31 +0800

    Add method abstraction

--- backend/src/main/java/com/rateNUS/backend/BackendApplication.java
+++ backend/src/main/java/com/rateNUS/backend/BackendApplication.java
@@ -4,6 +4,7 @@ import com.rateNUS.backend.model.Hostel;
 import com.rateNUS.backend.util.DummyData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 public class BackendApplication {
     @GetMapping
+    @CrossOrigin("http://localhost:8081/")
     public List<Hostel> getAllHostel() {
         return DummyData.hostelList;
     }
--- frontend/src/components/HostelList.vue
+++ frontend/src/components/HostelList.vue
@@ -3,13 +3,13 @@
         <h1>Hostel List</h1>
         <div v-for="hostel in hostelList" :key="hostel.id">
             <h3>{{hostel.id}}. {{hostel.title}}</h3>
-            <p>{{hostel.body}}</p>
+            <!-- <p>{{hostel.location}}</p> -->
         </div>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
+import HostelRequest from '../httpRequests/HostelRequest';
 export default {
     name:"HostelList",
     data() {
@@ -17,13 +17,26 @@ export default {
             hostelList: [],
         }
     },
+    methods: {
+        getHostelList() {
+            HostelRequest.getHostelList().then((response) => {
+                console.log(response.data);
+                this.hostelList = response.data.slice(0, 2);
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+    },
     mounted() {
-        axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-            console.log(response.data);
-            this.hostelList = response.data.slice(0, 2);
-        }).catch(error => {
-            console.log(error);
-        })
+
+        this.getHostelList();
+        // "https://jsonplaceholder.typicode.com/posts"
+        // axios.get("http://127.0.0.1:8080").then((response) => {
+        //     console.log(response);
+        //     this.hostelList = response.data.slice(0, 2);
+        // }).catch(error => {
+        //     console.log(error);
+        // })
     },
 }
 </script>
