93ce8282275ffa700d8d96f4156efbd090a082ee
SkyBlaise <zsc990124@gmail.com>
2021-12-09 17:45:09 +0800

    Sort list of comments by timestamp, latest to oldest

--- backend/src/main/java/com/rateNUS/backend/comment/CommentService.java
+++ backend/src/main/java/com/rateNUS/backend/comment/CommentService.java
@@ -4,6 +4,7 @@ import com.rateNUS.backend.util.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,6 +24,7 @@ public class CommentService {
 
     public List<Comment> getComments(long targetId, Type type) {
         List<Comment> comments = commentRepository.findByTargetIdAndType(targetId, type);
+        comments.sort(Comparator.comparing(Comment::getTimestamp).reversed());
 
         logger.log(Level.INFO, String.format("Found %d comments", comments.size()));
 
