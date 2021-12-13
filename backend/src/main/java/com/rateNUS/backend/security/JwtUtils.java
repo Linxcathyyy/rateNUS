aa4183a632d412a02193af8266880ab6162a8f5d
HongshuW <hongshu.wang.uni@gmail.com>
2021-12-14 00:50:37 +0800

    Implement JWT Add JwtResponse.java and JwtUtils.java Add jwtSecret and jwtExpirationMs to application.properties

---
+++ backend/src/main/java/com/rateNUS/backend/security/JwtUtils.java
@@ -0,0 +1,28 @@
+package com.rateNUS.backend.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class JwtUtils {
+    @Value("${ratenus.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${ratenus.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication) {
+        UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+}
