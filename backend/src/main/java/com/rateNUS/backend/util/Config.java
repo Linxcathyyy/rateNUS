261d5d0f8ca8f6c5f621010065fc04bb930daf73
SkyBlaise <zsc990124@gmail.com>
2021-12-15 18:15:35 +0800

    Combine 2 config file into 1 and move into util folder

--- backend/src/main/java/com/rateNUS/backend/util/Config.java
+++ backend/src/main/java/com/rateNUS/backend/util/Config.java
@@ -1,5 +1,23 @@
 package com.rateNUS.backend.util;
 
+import com.rateNUS.backend.comment.CommentRepository;
+import com.rateNUS.backend.hostel.HostelRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Provides initial sample data to database used by repositories.
+ */
+@Configuration
 public class Config {
     public static final String frontendURL = "http://localhost:8081/";
+
+    @Bean
+    CommandLineRunner commandLineRunner(HostelRepository hostelRepository, CommentRepository commentRepository) {
+        return args -> {
+            hostelRepository.saveAll(DummyData.hostelList);
+            commentRepository.saveAll(DummyData.commentList);
+        };
+    }
 }
