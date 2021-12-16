713f2f4d494002d94c3f3f862ed4213c165b171d
SkyBlaise <zsc990124@gmail.com>
2021-12-16 15:21:08 +0800

    Rename canteen to stall

---
+++ backend/src/main/java/com/rateNUS/backend/stall/Stall.java
@@ -0,0 +1,16 @@
+package com.rateNUS.backend.stall;
+
+import java.util.List;
+
+public class Stall {
+    private long id;
+    private String name;
+    private double rating;
+    private int commentCount;
+    private String location;
+    private String description;
+    private List<String> imageUrl;
+    private double lowestPrice;
+    private double highestPrice;
+
+}
