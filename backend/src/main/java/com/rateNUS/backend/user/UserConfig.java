8e3107eb4872ef02998a1faf5d560b9751bf5def
SkyBlaise <zsc990124@gmail.com>
2021-12-16 15:59:48 +0800

    Reformat backend

--- backend/src/main/java/com/rateNUS/backend/user/UserConfig.java
+++ backend/src/main/java/com/rateNUS/backend/user/UserConfig.java
@@ -1,10 +1,9 @@
 package com.rateNUS.backend.user;
 
+import com.rateNUS.backend.util.DummyData;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.rateNUS.backend.util.DummyData;
 
 /**
  * Provides initial sample data to database used by {@code UserRepository}.
