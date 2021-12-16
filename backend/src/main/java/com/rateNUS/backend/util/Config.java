0ddafb549d47cc4f6f86eb3969f50137a94dc9bf
SkyBlaise <zsc990124@gmail.com>
2021-12-16 15:25:26 +0800

    Add user config into util config

--- backend/src/main/java/com/rateNUS/backend/util/Config.java
+++ backend/src/main/java/com/rateNUS/backend/util/Config.java
@@ -3,6 +3,7 @@ package com.rateNUS.backend.util;
 import com.rateNUS.backend.comment.Comment;
 import com.rateNUS.backend.comment.CommentController;
 import com.rateNUS.backend.hostel.HostelRepository;
+import com.rateNUS.backend.user.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +16,10 @@ public class Config {
     public static final String frontendURL = "http://localhost:8081/";
 
     @Bean
-    CommandLineRunner commandLineRunner(HostelRepository hostelRepository, CommentController commentController) {
+    CommandLineRunner commandLineRunner(HostelRepository hostelRepository, CommentController commentController,
+                                        UserRepository userRepository) {
         return args -> {
+            userRepository.saveAll(DummyData.userList);
             hostelRepository.saveAll(DummyData.hostelList);
 
             for (Comment comment : DummyData.commentList) {
