5e7f4644cecef8387fcb9e2554bcd2680ad0821a
Ruofan1023 <e0563986@u.nus.edu>
2021-12-16 14:17:46 +0800

    Add persisted login state and logout feature

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,20 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import UserUtil from "../components/authentication/UserUtil";
+import createPersistedState from 'vuex-persistedstate'
+import SecureLS from "secure-ls";
+const ls = new SecureLS({ isCompression: false });
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+    plugins: [createPersistedState({
+        storage: {
+            getItem: key => ls.get(key),
+            setItem: (key, value) => ls.set(key, value),
+            removeItem: key => ls.remove(key)
+          }
+    })],
     state: {
         isLoggedIn: false,
         user: {
@@ -50,8 +60,10 @@ export default new Vuex.Store({
             state.isLoggedIn = true;
         },
         logOut(state) {
-            state.isLoggedIn = false
+            state.isLoggedIn = false;
+            state.user.jwtToken = "";
         }
+        
     },
     actions: {}
 });
