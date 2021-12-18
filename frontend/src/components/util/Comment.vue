1e3c55c340614839fe76c307474131731aef0d0b
JeffZincatz <jeffzincatz@gmail.com>
2021-12-18 15:31:26 +0800

    Add edit btn (not displayed yet) for future comment editing

--- frontend/src/components/util/Comment.vue
+++ frontend/src/components/util/Comment.vue
@@ -1,9 +1,23 @@
 <template>
   <div :key="comment.id" class="comment">
-    <div class="comment-info">
-      <Rating :score="comment.rating" />
-      <p>Posted on: {{ date }} {{ time }}</p>
-    </div>
+    <v-row>
+      <v-col>
+        <div class="comment-info">
+          <Rating :score="comment.rating" />
+          <p>Posted on: {{ date }} {{ time }}</p>
+        </div>
+      </v-col>
+      <v-col
+        align-self="center"
+        align="right"
+        class="mx-4"
+        v-show="this.isCurrentUser"
+      >
+        <v-btn icon color="orange accent-4">
+          <v-icon>mdi-pencil-outline</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
     <p class="description">
       {{ comment.text }}
     </p>
@@ -25,12 +39,13 @@ export default {
     return {
       date: null,
       time: null,
+      isCurrentUser: false, // will change based on user log in
     };
   },
   mounted() {
     this.date = new Date(this.comment.timestamp).toLocaleDateString();
     this.time = new Date(this.comment.timestamp).toLocaleTimeString();
-    console.log("mounted: ", this.comment);
+    // console.log("mounted: ", this.comment);
   },
 };
 </script>
