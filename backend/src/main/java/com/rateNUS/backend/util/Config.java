523e9954a7fa10ad9c1b6db449346faec557b812
HongshuW <hongshu.wang.uni@gmail.com>
2022-01-04 16:20:24 +0800

    Change create-drop to update

--- backend/src/main/java/com/rateNUS/backend/util/Config.java
+++ backend/src/main/java/com/rateNUS/backend/util/Config.java
@@ -1,11 +1,5 @@
 package com.rateNUS.backend.util;
 
-import com.rateNUS.backend.comment.Comment;
-import com.rateNUS.backend.comment.CommentController;
-import com.rateNUS.backend.hostel.HostelRepository;
-import com.rateNUS.backend.stall.StallRepository;
-import com.rateNUS.backend.studyarea.StudyAreaRepository;
-import com.rateNUS.backend.user.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,20 +12,7 @@ public class Config {
     public static final String frontendURL = "http://ratenus.com/";
 
     @Bean
-    CommandLineRunner commandLineRunner(HostelRepository hostelRepository,
-                                        StallRepository stallRepository,
-                                        StudyAreaRepository studyAreaRepository,
-                                        UserRepository userRepository,
-                                        CommentController commentController) {
-        return args -> {
-            userRepository.saveAll(DummyData.userList);
-            hostelRepository.saveAll(DummyData.hostelList);
-            stallRepository.saveAll(DummyData.stallList);
-            studyAreaRepository.saveAll(DummyData.studyAreaList);
-
-            for (Comment comment : DummyData.commentList) {
-                commentController.addComment(comment);
-            }
-        };
+    CommandLineRunner commandLineRunner() {
+        return args -> {};
     }
 }
