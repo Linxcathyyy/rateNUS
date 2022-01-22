5bb768a787ab50ed80e5ccc77f96781b5d8b4f85
HongshuW <hongshu.wang.uni@gmail.com>
2022-01-22 20:45:57 +0800

    Contribute page: update content

--- frontend/src/views/Contribute.vue
+++ frontend/src/views/Contribute.vue
@@ -15,7 +15,7 @@
     <p>Want to make an impact on the life of all NUS students?</p>
     <p>Join our team and contribute to our website!</p>
     <p>Talents we are looking for:</p>
-    <p>Administrators and Programmers</p>
+    <p>Administrators, Editors, Photographers, Programmers</p>
     <p>If you are interested, please send your CV to ratenus.info@gmail.com</p>
     <br>
     <p class="subtitle">
@@ -42,29 +42,30 @@
         Pull Request
     </p>
     <p>Please create a GitHub pull request if you want to improve our code.</p>
-    <a href="https://github.com/Linxcathyyy/rateNUS/pulls">Create a pull request here</a>
+    <div class="link" @click="redirectToGitHubPRs()">Create a pull request here</div>
     <p>We will review your request and update the changes to our website if accepted!</p>
     <br>
     <p class="subtitle">
         Report Bug
     </p>
     <p>If you spot a bug in our website, please report it here:</p>
-    <a href="https://github.com/Linxcathyyy/rateNUS/issues">Report a bug here</a>
+    <div class="link" @click="redirectToGitHubIssues()">Report a bug here</div>
     <p>Alternatively you can send the description of the bug through email to ratenus.info@gmail.com</p>
     <p>We will fix the bug as soon as possible.</p>
 
     <br>
     <br>
-    <a href="https://www.flaticon.com/free-icons/contact-us" title="contact us icons" class="link">Contact us icons created by alimasykurm - Flaticon</a>
+    <a href="https://www.flaticon.com/free-icons/contact-us" title="contact us icons" class="attribute">Contact us icons created by alimasykurm - Flaticon</a>
     <br>
-    <a href="https://www.flaticon.com/free-icons/code" title="code icons" class="link">Code icons created by prettycons - Flaticon</a>
+    <a href="https://www.flaticon.com/free-icons/code" title="code icons" class="attribute">Code icons created by prettycons - Flaticon</a>
   </div>
 </template>
 
 <script>
+import GithubRequest from "../httpRequests/GithubRequest.js";
 export default {
     computed: {
-          imageHeight() {
+        imageHeight() {
           switch (this.$vuetify.breakpoint.name) {
             case "xs":
               return 50;
@@ -80,7 +81,15 @@ export default {
               return 80;
           }
         },
-      }
+    },
+    methods: {
+        redirectToGitHubPRs() {
+            window.open(GithubRequest.getPullRequestUrl());
+        },
+        redirectToGitHubIssues() {
+            window.open(GithubRequest.getIssuesUrl());
+        }
+    }
 };
 </script>
 
@@ -89,7 +98,7 @@ export default {
         font-size: 1.3em;
         font-weight: bold
     }
-    .link {
+    .attribute {
         font-size: 0.8em
     }
 </style>
